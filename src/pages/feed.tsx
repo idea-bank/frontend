27c43e5eed576db0c3f197a71155e5641c74c654
Matthew Isayan <misayan@gmail.com>
2023-02-15 03:43:28 -0800

    Temp fixes to image heights for standardization

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -4,6 +4,7 @@ import MOCK_DATA from "@/data/MOCK_DATA.json"
 import Post from '@/components/post';
 import MobileNav from '@/components/MobileNav';
 import { useMediaQuery } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const getPosts = () : PostModel[] => {
     const postArr : PostModel[] = MOCK_DATA;
@@ -13,9 +14,18 @@ const getPosts = () : PostModel[] => {
 }
 export default function Feed() {
     const isMobile = useMediaQuery('(min-width:425px)');
+    const [height, setHeight] = useState(0);
+    useEffect(() => {
+      function handleResize() {
+        setHeight(window.innerHeight);
+      }
+      handleResize(); 
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
     return (
         <>
-        <div className={styles.feed}>
+        <div className={styles.feed} style={{height :height}}>
             { getPosts().map((post : PostModel, index)=>{
                 return <Post 
                 post={post}
