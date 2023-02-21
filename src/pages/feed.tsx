4201dd434441818c927b877a21027ba1545e3e6a
Matthew Isayan <misayan@gmail.com>
2023-02-20 21:25:24 -0800

    Removed unused imports

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -2,10 +2,6 @@ import styles from '../styles/feed.module.css';
 import { PostModel } from "@/models/PostModel"
 import MOCK_DATA from "@/data/MOCK_DATA.json"
 import Post from '@/components/post';
-import MobileNav from '@/components/MobileNav';
-import { useMediaQuery } from '@mui/material';
-import { useEffect, useState } from 'react';
-import Navbar from '@/components/navbar';
 import useWindowHeight from '@/hooks/window-height';
 
 const getPosts = () : PostModel[] => {
