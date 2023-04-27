c3af7c82798d2ec8c7dc9861ec9b0d4cbb47feae
Matthew <misayan@gmail.com>
2023-04-26 23:37:11 -0700

    Improving desktop ux

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,25 +1,30 @@
-import styles from '../styles/feed.module.css';
-import { PostModel } from "@/models/PostModel"
-import MOCK_DATA from "@/data/MOCK_DATA.json"
-import Post from '@/components/post';
-import useWindowHeight from '@/hooks/window-height';
+import styles from "../styles/feed.module.css";
+import { PostModel } from "@/models/PostModel";
+import MOCK_DATA from "@/data/MOCK_DATA.json";
+import Post from "@/components/post";
+import useWindowHeight from "@/hooks/window-height";
+import { useIsSmall } from "@/hooks/media-queries";
 
-const getPosts = () : PostModel[] => {
-    const postArr : PostModel[] = MOCK_DATA;
-   
+const getPosts = (): PostModel[] => {
+  const postArr: PostModel[] = MOCK_DATA;
 
-    return postArr;
-}
+  return postArr;
+};
 export default function Feed() {
-    const height = useWindowHeight();
-    return (
-        <div className={styles.feed} style={{height :height}}>
-            { getPosts().map((post : PostModel, index)=>{
-                return <Post 
-                post={post}
-                key={index}
-                />   
-            })}
-        </div>
-  )
+  const height = useWindowHeight();
+  const isMobile = useIsSmall();
+  return (
+    <div
+      className={styles.feed}
+      style={{
+        height: isMobile ? height : "auto",
+        marginTop: !isMobile ? "3%" : 0,
+        scrollSnapType: isMobile ? "y mandatory" : "inherit",
+      }}
+    >
+      {getPosts().map((post: PostModel, index) => {
+        return <Post post={post} key={index} />;
+      })}
+    </div>
+  );
 }
