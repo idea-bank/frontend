c4b47c18453d4f9ae13c2128884eed54e02eee52
Matthew <misayan@gmail.com>
2023-04-26 22:23:45 -0700

    Added SideNav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import useMediaQuery from "@mui/material/useMediaQuery"
 import MobileNav from "./MobileNav";
+import SideNav from "./side-nav";
 
 export default function Navbar() {
     const isMobile = useMediaQuery('(max-width:800px)');
     return(
         <>
-            { isMobile ? <MobileNav/> : <></>}
+            { isMobile ? <MobileNav/> : <SideNav/>}
         </>
     )
 }
