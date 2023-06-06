e3f48bbe960dc5be62747b006feef27a5a54821b
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-06-06 14:15:20 -0700

    Patch config

--- next.config.js
+++ next.config.js
@@ -10,18 +10,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_ROOT_URL}/:path*`,
-      },
-      {
-        source: "/api/v2/:path*",
-        destination: `${process.env.NEXT_PUBLIC_ROOT_URL_V2}/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
